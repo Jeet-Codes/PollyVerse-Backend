fa514a271c90802da89812c46678b3c485143de3
Sandeep Kumar <sandeepkumar090304@gmail.com>
2024-04-18 09:53:22 +0530

    Initial commit

---
+++ src/main/java/org/example/pollyversebackend/PollyVerseBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.pollyversebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollyVerseBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PollyVerseBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/pollyversebackend/PollyVerseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pollyversebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollyVerseBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
