bfec5e687f8bd146ebe7ae2c57f03ad9a18563f2
Sandeep Kumar <sandeepkumar090304@gmail.com>
2024-04-25 20:57:16 +0530

    Added Swagger API Docs

---
+++ src/main/java/org/example/pollyversebackend/Config/DocsConfig.java
@@ -0,0 +1,21 @@
+package org.example.pollyversebackend.Config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
+
+@OpenAPIDefinition
+        (info = @Info(title = "PolyVerse Backend Api",
+                                description = "This Api is For Backend API's",
+                                summary = "API Contains Summary Info",
+                                termsOfService = "Term&Condition Applied..",
+                                contact = @Contact(name = "Developed By Jeet-Codes",url = "https://github.com/Jeet-Codes"),
+                                version = "v1",
+                                license = @License(name = "MIT Licence 1.0")
+            )
+        )
+public class DocsConfig {
+
+}
